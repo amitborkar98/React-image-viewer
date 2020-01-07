600c1e3328fdc3b3034456f14ef29a63c3fb8dbc
amitborkar98 <amitborkar98@gmail.com>
2020-01-07 22:53:25 +0530

    Added the layout for comments section

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,6 +9,11 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
 class Home extends Component{
     render(){
         return(
@@ -49,7 +54,20 @@ class Home extends Component{
                                     <FavoriteIcon/>
                                     <FavoriteBorderIcon/><span>7 Likes</span>
                                 </div>
-                                
+                                <br/>
+                                <Typography variant="body2" component="p" >
+                                    new added
+                                </Typography> 
+                                <br/>
+                                <div className="comments">
+                                    <FormControl className="control">
+                                        <InputLabel htmlFor="movieName">Add a comment</InputLabel>
+                                        <Input id="movieName" />
+                                    </FormControl>
+                                    <Button variant="contained" color="primary" >
+                                        ADD
+                                    </Button>
+                                </div>
                             </CardContent>
                         </div>   
                     </Card>
@@ -64,18 +82,48 @@ class Home extends Component{
                                         R
                                     </Avatar>
                                     }
-                                    title="Username"
+                                    title="username"
                                     subheader="September 14, 2016"
-                                    />
+                                />
                             </div>
                             <CardContent>
-                                Post1             
+                                Post1 
+                                <CardMedia
+                                    className="media"
+                                    image="../../assests/"
+                                    title="Paella dish"
+                                />      
+                                <hr/>
+                                <Typography variant="body1" component="p">
+                                    Add 1 cup of frozen peas along with the mussels, if you like.
+                                </Typography>
+                                <Typography variant="body2" component="p" className="hastag">
+                                    #football
+                                </Typography> 
+                                <br/>    
+                                <div className="likes">
+                                    <FavoriteIcon/>
+                                    <FavoriteBorderIcon/><span>7 Likes</span>
+                                </div>
+                                <br/>
+                                <Typography variant="body2" component="p" >
+                                    new added
+                                </Typography> 
+                                <br/>
+                                <div className="comments">
+                                    <FormControl className="control">
+                                        <InputLabel htmlFor="movieName">Add a comment</InputLabel>
+                                        <Input id="movieName" />
+                                    </FormControl>
+                                    <Button variant="contained" color="primary" >
+                                        ADD
+                                    </Button>
+                                </div>
                             </CardContent>
                         </div>   
                     </Card>
                     
                 </div>
-
             </div>
         );
     }
